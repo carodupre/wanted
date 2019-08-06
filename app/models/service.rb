1e8da12a2a3d1a457ac94f41dab84e93fa4547fa
caroline dupre <duprecar@gmail.com>
2019-08-06 18:10:10 +0800

    adding image uploader, installing carrierwave and cloudinary for services forms and views

--- app/models/service.rb
+++ app/models/service.rb
@@ -8,4 +8,5 @@ class Service < ApplicationRecord
   # validation for price_per_hour must be checked, we don't know if it works well or not
   validates :title, uniqueness: { scope: :location }
   # validation uniqueness of the couple title/location must be checked as well
+  mount_uploader :photo, PhotoUploader
 end
