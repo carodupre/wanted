dc383f4a556b2dcbf23f0b71609190ea33293be6
caroline dupre <duprecar@gmail.com>
2019-08-06 17:40:19 +0800

    Add dotenv - Protect my secret data in .env file

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,44 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update]
+
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user_id = current_user.id
+    if @booking.save
+      redirect_to dashobard
+    else
+      render "new"
+    end
   end
 
   def edit
   end
 
-  def update
+   def update
+      @booking.update(booking_params)
+      if @booking.save
+        redirect_to dashboard
+      else
+        render "edit"
+      end
+    end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
+
+  def booking_params
+    params.require(:booking).permit(:duration, :total_price, :address, :service_id)
+  end
+
 end
