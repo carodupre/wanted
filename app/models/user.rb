30f8c53a8711649cd1898ef5e07b3cf4e1f173e9
Austen Bright <pureaustenbright@gmail.com>
2019-08-06 06:12:53 -0400

    worked on user controllers and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  mount_uploader :avatar, AvatarUploader
   has_many :bookings
   has_many :services, through: :bookings
   validates :username, :phone_number, presence: true
