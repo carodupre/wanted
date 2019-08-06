63ab5f9594c50b946f0b500da1fe647c08a4870b
kenny bergounhe <bergounhe.kenny.code@gmail.com>
2019-08-06 11:47:58 +0800

    routes done, controllers with empty methods done

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-
   resources :services do
-    resources :bookings, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:show, :edit, :update] do
       resources :reviews, only: [:create]
     end
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
