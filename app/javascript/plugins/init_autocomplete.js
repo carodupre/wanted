ea9ab68389732d564d45a02f6406fee747487e40
caroline dupre <duprecar@gmail.com>
2019-08-08 13:09:09 +0800

    added auto-complete in bookings form

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,6 +1,11 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
+  const addressInput = document.getElementById('booking_address');
+  if (addressInput) {
+    places({ container: addressInput });
+  }
+
   const locationInput = document.getElementById('service_location');
   if (locationInput) {
     places({ container: locationInput });
