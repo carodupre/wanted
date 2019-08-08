3fb9cf70379cb09f180594442fdfb1d2c9b439ed
martyn hickey <martynhickey11@gmail.com>
2019-08-08 12:47:43 +0800

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,7 +51,7 @@ puts 'Creating 20 fake services...'
     description: Faker::Job.field,
     user: User.all.sample,
     category: Category.all.sample,
-    photo: photo: Faker::LoremFlickr.image
+    photo:
   )
   service.save!
 end
